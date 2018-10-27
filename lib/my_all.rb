3ba1c0711b935e62c00babdc0a157e32c693382b
Sung Bae <singsang2@gmail.com>
2018-10-27 23:37:28 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  counter = 0
+  collect = []
+  while counter < collection.length
+    collect << yield(collection[counter])
+    counter += 1
+  end
+  collect.include?(false) ? false : true
 end
