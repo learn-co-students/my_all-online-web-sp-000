86c8141b275e6f9daf2c769f93686cfa532b3e88
Ariel <arielord@buffalo.edu>
2018-12-12 19:32:28 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  while i < collection.length do
+    if yield(collection[i])
+      i += 1
+    else
+      return false
+    end
+  end
+  true
 end
