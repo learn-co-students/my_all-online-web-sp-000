65c043f28ec0cec105d256c6e55fd1c60dd0b73b
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-01-27 02:07:04 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+    i = 0
+    block_return_values = []
+    i += 1
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
