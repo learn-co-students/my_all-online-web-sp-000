fe55a67f84dc396986df9b57d3f017e4d82ccb3a
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-28 06:25:49 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
+   i = 0 
+   values = [ ]
+  while i < collection.length 
+    values << yield(collection[i])
+    i +=1
+  end
+  if values.include?(false) 
+    false 
+  else 
+    true 
+  end 
+end
 
+my_all?([1, 2, 3,]) do |num|
+  num < 2
 end
