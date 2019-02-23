e5324a5bd6bf8d82a842769a495e711f6e94a2a0
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-23 00:32:25 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
-
+  new_array = []
+  i = 0 
+while i < collection.length
+new_array << yield(collection[i])
+i += 1 
 end
+new_array.include?(false) ? false : true
+end
+
