da0aec62cbb747b4303a3ff8a3cc0a8b973a2455
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 04:01:53 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  altered_array = []
+    while i < collection.length
+      altered_array << yield(collection[i])
+      i = i + 1
+    end
 
+    if altered_array.include? (false)
+      false
+    else
+      true
+    end
 end
