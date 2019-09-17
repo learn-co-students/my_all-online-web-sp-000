da2055951ca898355478491d26de2a072497df30
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-17 18:15:14 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+  # print block_return_values #this print changes the return value of this whole method to nil(so be careful using it!!)
 end
+
+my_all?([1, 2, 3]) {|i| i > 0}
+# my_all?([true, true, true]) {|i| i < 2}
