6347a927ff66c96b4fb18d5c9aefd14460c6cbcf
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-19 19:14:26 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
-
+  counter = 0
+  all_true = true
+  while counter < collection.length
+    current_true = yield collection[counter]
+    if !current_true
+      all_true = false
+      break
+    end
+    counter += 1
+  end
+  all_true
 end
+
+puts my_all?([1,2,3]) {|i| i < 4}
