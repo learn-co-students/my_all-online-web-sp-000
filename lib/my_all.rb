89275a9b47bed0fb5c4ec52f3f8f4ff1e81439ac
Doak Covington <doakcovington@protonmail.com>
2020-01-02 03:26:25 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,16 +2,18 @@ require 'pry'
 
 def my_all?(collection)
   binding.pry
-  i = 0
-  block_return_values = []
-  while i < collection.length
-    block_return_values << yield(collection[i])
-    i = i + 1
-  end
+  i = 0 # set counter to 0
 
-  if block_return_values.include?(false)
-    false
-  else
-    true
-  end
-end
+  block_return_values = [] #array containing values from loop
+
+  while i < collection.length #while i is less than collection length
+    block_return_values << yield(collection[i]) #add curent value to block_return_values
+    i = i + 1 #increase i by 1
+  end #end of while loop
+
+  if block_return_values.include?(false) #if block_return_values includes a false value
+    false #return false
+  else #else
+    true #return true
+  end #end of if statement
+end #end of my_all? method
