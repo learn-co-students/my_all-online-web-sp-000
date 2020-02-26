9103f1339d8ba1e34f438aa797659368db54576d
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-26 02:14:24 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
-
+   if block_given?
+      counter = 0
+      array = []
+      while counter < collection.length
+         array <<  yield(collection[counter])
+           counter += 1
+         end
+         if array.include?(false)
+          false
+         else
+           true
+         end
+    else
+      puts  "No array was pass!"
+    end
 end
