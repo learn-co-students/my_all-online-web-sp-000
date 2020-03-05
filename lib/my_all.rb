5002b8c826f0860b9dd0eeda14de2edd5c11c508
Xiaoyi Liu <lxy878@gmail.com>
2020-03-05 14:21:19 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  results = []
+  i = 0
+  while i < collection.size
+    results << yield(collection[i])
+    i += 1
+  end
+  if results.include?(false)
+    false
+  else
+    true
+  end
 end
