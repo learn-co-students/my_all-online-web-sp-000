0f0cbab86f6d46a65716614b4dcf67238a5322be
Mark Stabler <mark@stablerwritingservices.com>
2020-03-06 19:42:16 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
+  all_true = true
 
+  i = 0
+  while i < collection.length
+    if yield(collection[i]) == false
+      all_true = false
+    end
+    i += 1
+  end
+
+  all_true
 end
