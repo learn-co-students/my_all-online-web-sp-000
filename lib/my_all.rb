c9f19800226f07663b588b9edd87735ad55ad8b8
Mark Stabler <mark@stablerwritingservices.com>
2020-03-06 19:54:50 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
-def my_all?(collection)
+# my original solution (works but not as abstract so less modular, not better because shorter)
+def my_all_orig?(collection)
   all_true = true
 
   i = 0
@@ -8,8 +9,30 @@ def my_all?(collection)
     if yield(collection[i]) == false
       all_true = false
     end
-    i += 1
+  i += 1
   end
 
   all_true
+end
+
+
+# school solution - more abstract so modular
+#      i.e. this could easily be changed to mimic the .none method by simply
+#      changing the .include conditional to look for true instead of false
+#           example: if block_return_values.include?(true)
+# my solution would require changing multiple conditions/values and the variable name for clairity
+def my_all?(collection)
+  block_return_values = []
+
+  i = 0
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
