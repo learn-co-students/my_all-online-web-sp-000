0c0a4558fb14c5c3565895b78a2a1f91de01b6b8
Ian Mayton <ian.mayton@gmail.com>
2020-07-18 14:36:57 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  counter = 0
+  returned_collection = []
+  while counter < collection.length 
+    returned_collection << yield(collection[counter])
+    counter += 1
+  end
+  if returned_collection.include? (false)
+    false
+  else
+    true 
+  end
 end
