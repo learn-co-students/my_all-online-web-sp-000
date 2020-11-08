272f09ac1d9903072b8d0d80dab7770015bb42be
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-08 19:07:59 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
+count = 0
 
+array = []
+  while count < collection.length
+   array << yield(collection[count])
+  count += 1
+
+  end
+  if array.include?(false)
+    false
+  else 
+    true
+  end
 end
