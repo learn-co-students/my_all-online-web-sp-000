8057874cc258abc3891e536019da866027151ef0
Hannah Malm <hannahemalm616@gmail.com>
2020-12-01 00:33:30 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
-
+ i = 0 
+ block_return_values = [] #create an empty array 
+ while i < collection.length 
+ block_return_values <<
+ #when this code is run and it hits the yield line, it is going to send whatever is passed in as the argument to the block 
+  yield(collection[i])
+ i = i + 1  
 end
+
+if
+  block_return_values.include?(false)
+    false
+  else  
+    true  
+  end
+end  
