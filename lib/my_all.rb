95872aeb27cd88125b97d6318f7a0ab4ec7eeffb
Jeff Bai <j.bai@wustl.edu>
2021-01-10 05:39:49 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
-def my_all?(collection)
-
+def my_all?(array)
+  collection = []
+  i = 0
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection.include?(false) ? false : true
 end
