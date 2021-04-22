b19a882d7a7224673113a8f7d7e81339e77a5021
pekellogg <pekellogg@hotmail.com>
2021-04-21 17:02:44 -0700

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,20 @@
 require 'pry'
 
 def my_all?(collection)
+    i = 0
+    block_return_values = []
+    while i < collection.length
+        if block_given?
+            block_return_values << yield(collection[i])
+            i += 1
+        else
+            return "No block given to yield in my_all method."
+        end
+    end
 
+    if block_return_values.include?(false)
+        false
+    else
+        true
+    end
 end
