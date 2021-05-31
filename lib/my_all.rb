8bb6a669fe6876e38555e3fa78d28ba7d3ea0a51
luis leon <leonluis380@yahoo.com>
2021-05-31 01:44:11 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  new_ary = Array.new
+  while i < collection.size
+    new_ary << yield(collection[i])
+    i+=1
+  end
 
+  if new_ary.include?(false)
+    false
+  else
+    true
+  end
 end
